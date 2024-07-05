5ed9bad6ba47dd65802b78b3dde852599695d627
Jordan Phillips <jordan@amplenote.com>
2024-07-05 10:46:22 -0700

    Add excalidraw

--- src/embed.jsx
+++ src/embed.jsx
@@ -1,5 +1,13 @@
 import React from "react"
+import { Excalidraw } from "@excalidraw/excalidraw"
 
 export default function Embed() {
-  return (<h1>Hello, world!</h1>);
+  return (
+    <>
+      <h1>Hello, Excalidraw!</h1>
+      <div style={ { height: "500px" } }>
+        <Excalidraw />
+      </div>
+    </>
+  );
 }
