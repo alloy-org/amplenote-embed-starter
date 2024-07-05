370963ce37825ac17a6a9ab2e723d0fdd4f742ab
Jordan Phillips <jordan@amplenote.com>
2024-07-05 13:05:12 -0700

    Send data to plugin on change

--- src/embed.jsx
+++ src/embed.jsx
@@ -1,12 +1,24 @@
+import debounce from "lodash.debounce"
+import { Excalidraw, serializeAsJSON } from "@excalidraw/excalidraw"
 import React from "react"
-import { Excalidraw } from "@excalidraw/excalidraw"
+
+const onChange = debounce(
+  (elements, appState, _files) => {
+    const data = serializeAsJSON(elements, appState);
+    window.callAmplenotePlugin("change", data);
+  },
+  1000,
+  { maxWait: 10000 }
+);
 
 export default function Embed() {
   return (
     <>
       <h1>Hello, Excalidraw!</h1>
       <div style={ { height: "500px" } }>
-        <Excalidraw />
+        <Excalidraw
+          onChange={ onChange }
+        />
       </div>
     </>
   );
