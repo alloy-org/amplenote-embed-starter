2a28784dd3df24160e933695e4ded59ab46b7507
Jordan Phillips <jordan@bonanza.com>
2024-08-23 12:37:48 -0700

    Add saving indicator

--- src/embed.jsx
+++ src/embed.jsx
@@ -1,18 +1,11 @@
-import debounce from "lodash.debounce"
 import { Excalidraw, serializeAsJSON } from "@excalidraw/excalidraw"
-import React from "react"
+import debounce from "lodash.debounce"
+import React, { useMemo, useRef, useState } from "react"
 import { useAsyncEffect } from "@react-hook/async"
 
-const onChange = debounce(
-  (elements, appState, _files) => {
-    const data = serializeAsJSON(elements, appState);
-    window.callAmplenotePlugin("change", data);
-  },
-  1000,
-  { maxWait: 5000 }
-);
-
 export default function Embed() {
+  const [ isSaving, setIsSaving ] = useState(false);
+
   const { error, status, value } = useAsyncEffect(
     async () => {
       const data = await window.callAmplenotePlugin("load");
@@ -21,16 +14,56 @@ export default function Embed() {
     []
   );
 
+  const saveCounterRef = useRef(1);
+
+  const onChange = useMemo(
+    () => {
+      const save = debounce(
+        (elements, appState, _files, expectedSaveCounter) => {
+          const data = serializeAsJSON(elements, appState);
+
+          (async () => {
+            try {
+              await window.callAmplenotePlugin("change", data);
+            } finally {
+              if (saveCounterRef.current === expectedSaveCounter) {
+                setIsSaving(false);
+              }
+            }
+          })();
+        },
+        1000,
+        { maxWait: 5000 }
+      );
+
+      return (elements, appState, _files) => {
+        setIsSaving(true);
+        saveCounterRef.current += 1;
+        save(elements, appState, _files, saveCounterRef.current);
+      };
+    },
+    []
+  )
+
   if (status === "loading") {
     return (<div>loading</div>);
   } else {
     return (
       <>
-        <div style={ { height: "500px" } }>
+        <div style={ { height: "500px", position: "relative" } }>
           <Excalidraw
             initialData={ value }
             onChange={ onChange }
           />
+          {
+            isSaving
+              ? (
+                <div style={ { color: "#8593A3", fontSize: 14, left: 0, top: 0, position: "absolute", zIndex: 100 } }>
+                  saving...
+                </div>
+              )
+              : null
+          }
         </div>
       </>
     );
